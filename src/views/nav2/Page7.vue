55d7b744ea757959ea6a477249a229469a091574
mengzhaoyu <915016543@qq.com>
2018-08-18 23:46:00 +0800

    m

---
+++ src/views/nav2/Page7.vue
@@ -0,0 +1,57 @@
+<template>
+	<section>
+	{{sd}}
+	</br>
+	{{s}}
+	</section>
+</template>
+
+<script>
+	export default{
+		data() {
+			return {
+				sd:{},
+				s:{}
+			}
+		},
+		mounted(){
+			
+			{
+				let data = {
+					latitude:30.2735,
+					longitude:120.15526
+				}
+				this.$axios.get('api/restapi/bgs/poi/reverse_geo_coding',data).then(res=>{
+					console.log(res)
+					this.sd = res.data
+				}).catch(err=>{
+					console.log(err)
+				})
+			}
+			
+			
+			
+			{
+				let data = {
+					latitude:'30.273954',
+					longitude:'120.153968',
+					terminal:'h5'
+				}
+				this.$axios.get('api/restapi/shopping/v1/restaurants/outside_filter/attributes',data).then(res=>{
+					console.log(res)
+					this.s = res.data
+				}).catch(err=>{
+					console.log(err)
+				})
+			}
+			
+			
+			
+			
+		}
+	}
+</script>
+
+<style>
+	
+</style>
